f129175bed244c36b0775b9fbb19199ed9a02f83
moyvafish <fate@vocadev.ru>
2022-09-09 13:28:03 +0700

    main

---
+++ README.md
@@ -0,0 +1,1 @@
+"# 39-ws-manifest" 
---
+++ vkcs_provider.tf
@@ -0,0 +1,22 @@
+terraform {
+    required_providers {
+        vkcs = {
+            source = "vk-cs/vkcs"
+        }
+    }
+}
+
+provider "vkcs" {
+    # Your user account.
+    username = "fate@vocadev.ru"
+
+    # The password of the account
+    password = "YOUR_PASSWORD"
+
+    # The tenant token can be taken from the project Settings tab - > API keys.
+    # Project ID will be our token.
+    project_id = "3783352dba4b42e28914b4615649de4b"
+    
+    # Region name
+    region = "RegionOne"
+}
